e30330a0cf48e294821432e5b1518329854f0ca5
gabriel <gfluz94@gmail.com>
2023-01-29 22:56:56 -0300

    comment tbd methods due to linting

--- serve/predictor.py
+++ serve/predictor.py
@@ -30,7 +30,9 @@ class PredictorService(object):
             XGBClassifier: Trained classifier
         """
         if self._model is None:
-            with open(os.path.join(self._model_path, f"default_classifier.pkl"), "rb") as f:
+            with open(
+                os.path.join(self._model_path, "default_classifier.pkl"), "rb"
+            ) as f:
                 self._model = dill.load(f)
 
         return self._model
@@ -44,27 +46,23 @@ class PredictorService(object):
         """
         return pd.DataFrame([request])
 
-    def _preprocess_input_df(
-        self, df: pd.DataFrame
-    ) -> pd.DataFrame:
-        """Method to preprocess input dataframe in a format accepted by the model
-        Args:
-            df (pandas.DataFrame): Input dataframe containing variables
-        Returns:
-            pandas.DataFrame: Preprocessed dataframe to be fed to the model
-        """
-        pass
+    # def _preprocess_input_df(self, df: pd.DataFrame) -> pd.DataFrame:
+    #     """Method to preprocess input dataframe in a format accepted by the model
+    #     Args:
+    #         df (pandas.DataFrame): Input dataframe containing variables
+    #     Returns:
+    #         pandas.DataFrame: Preprocessed dataframe to be fed to the model
+    #     """
+    #     pass
 
-    def _convert_probability_to_score(
-        self, probability: float
-    ) -> float:
-        """Method to convert output probability into credit score
-        Args:
-            probability (float): Default probability output by the model
-        Returns:
-            float: Final credit score
-        """
-        pass
+    # def _convert_probability_to_score(self, probability: float) -> float:
+    #     """Method to convert output probability into credit score
+    #     Args:
+    #         probability (float): Default probability output by the model
+    #     Returns:
+    #         float: Final credit score
+    #     """
+    #     pass
 
     def predict(self, request_inputs: Dict[str, Any]) -> Dict[str, float]:
         """Method for prediction whenever a request invokes the model in production
@@ -73,11 +71,13 @@ class PredictorService(object):
         Returns:
             Dict[str, float]: Final prediction for default probability and credit score
         """
-        predictions = {
-            "uuid": request_inputs["uuid"]
-        }
+        predictions = {"uuid": request_inputs["uuid"]}
         X = self._convert_to_pandas(request_inputs)
-        X = self._preprocess_input_df(X)
-        predictions["default_probability"] = self._get_model().predict_proba(X)[:, 1].tolist()[0]
-        predictions["credit_score"] = self._convert_probability_to_score(predictions["default_probability"])
+        # X = self._preprocess_input_df(X)
+        predictions["default_probability"] = (
+            self._get_model().predict_proba(X)[:, 1].tolist()[0]
+        )
+        # predictions["credit_score"] = self._convert_probability_to_score(
+        #     predictions["default_probability"]
+        # )
         return predictions
